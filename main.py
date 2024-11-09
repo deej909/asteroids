7342f77f16f57c62070d34ca2e7f9e0f143a01db
deej909 <deej909is@gmail.com>
2024-11-09 20:38:12 +0800

    drawing static player triangle sprite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 from constants import *
 
@@ -8,15 +9,27 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #setup game clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #setup screen object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #setup player
+    player = Player( SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
